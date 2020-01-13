c0f01a0127dc4e60920619041aad20b9d92694ab
Riptod <otoval15>
2020-01-13 19:18:15 +0200

    added logger

--- src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
@@ -3,15 +3,18 @@ package mate.academy.internetshop.controller;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import mate.academy.internetshop.lib.Injector;
+import org.apache.log4j.Logger;
 
 public class InjectInitializer implements ServletContextListener {
+    final static Logger LOGGER = Logger.getLogger(InjectInitializer.class);
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
+            LOGGER.info("Dependency injection started");
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+            LOGGER.error(e);
         }
     }
 
