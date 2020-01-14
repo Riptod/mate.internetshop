44d90f462108edf634541e0de1017daa89998171
Riptod <otoval15>
2020-01-14 11:13:56 +0200

    fixed logger

--- src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
@@ -3,18 +3,15 @@ package mate.academy.internetshop.controller;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import mate.academy.internetshop.lib.Injector;
-import org.apache.log4j.Logger;
 
 public class InjectInitializer implements ServletContextListener {
-    final static Logger LOGGER = Logger.getLogger(InjectInitializer.class);
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
-            LOGGER.info("Dependency injection started");
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
-            LOGGER.error(e);
+            e.printStackTrace();
         }
     }
 
--- src/main/java/mate/academy/internetshop/lib/Injector.java
+++ src/main/java/mate/academy/internetshop/lib/Injector.java
@@ -7,10 +7,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
-import org.apache.log4j.Logger;
 
 public class Injector {
-    final static Logger LOGGER = Logger.getLogger(Injector.class);
     private static final String PROJECT_MAIN_PACKAGE = "mate.academy.internetshop";
     private static List<Class> classes = new ArrayList<>();
 
@@ -18,7 +16,7 @@ public class Injector {
         try {
             classes.addAll(getClasses(PROJECT_MAIN_PACKAGE));
         } catch (ClassNotFoundException | IOException e) {
-            LOGGER.error(e);
+            e.printStackTrace();
         }
     }
 
