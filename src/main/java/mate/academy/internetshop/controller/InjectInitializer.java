8f99fc0f42c4e343cf02cb8741f3953cccde3843
Riptod <57630394+Riptod@users.noreply.github.com>
2020-01-15 13:19:57 +0200

    added logger (#3)
    * added logger
    
    * fixed logger
    
    * fixed logger2
    
    * fixed logger3

--- src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
@@ -3,15 +3,19 @@ package mate.academy.internetshop.controller;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import mate.academy.internetshop.lib.Injector;
+import org.apache.log4j.Logger;
 
 public class InjectInitializer implements ServletContextListener {
+    private static final Logger LOGGER = Logger.getLogger(InjectInitializer.class);
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
+            LOGGER.error("Can't initialize all dependencies", e);
             throw new RuntimeException(e);
+
         }
     }
 
